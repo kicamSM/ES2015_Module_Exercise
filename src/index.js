88333b717a21bfe5ed857cda9b6f853d0fcd2704
kicamSM <kicamSMM@hotmail.com>
2023-09-01 14:39:44 -0600

    exercise

---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+import fruits from './foods'
+import { choice, remove } from './helpers'
+
+let fruit = choice(fruits);
+
+console.log(`I'd like one ${fruit}, please`);
+console.log(`Here you go: ${fruit}`);
+console.log(`Delicious! May I have another?`);
+
+let remaining = remove(fruit, fruits);
+console.log('remaining:', remaining)
+
+console.log(`I'm sorry, we're all out. We have ${remaining.length} left.`);
